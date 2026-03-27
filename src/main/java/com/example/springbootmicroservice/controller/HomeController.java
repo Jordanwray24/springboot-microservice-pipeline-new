53e0e01f8190f1aa2aaa48e534319b709639a970
Jordanwray24 <jordanwray24@outlook.com>
2026-03-27 17:00:11 -0400

    Add HomeController endpoint

---
+++ src/main/java/com/example/springbootmicroservice/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springbootmicroservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot app deployed successfully on AWS ECS!";
+    }
+}
